2cbe155eb47b36bcfc3c25b0a9d63a4e41859fda
Thakurayush124 <159556297+Thakurayush124@users.noreply.github.com>
2024-07-01 19:32:37 +0530

    tt

--- src/Home.js
+++ src/Home.js
@@ -53,10 +53,10 @@ function Home() {
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
             <nav className="navbar">
-                <div class="left">
+                <div className="left">
                     <img src={logo} alt="Logo" className="logo" />
                 </div>
-                <div class="right">
+                <div className="right">
                     <ul className="navbar-list">
                         <li className="navbar-item"><a href="/">Home</a></li>
                         <li className="navbar-item"><a href="/about">About</a></li>
@@ -90,7 +90,7 @@ function Home() {
                 < p >Get in touch</p>
                 <button onClick={handleWhatsAppClick}>
     <p>Say "Hi!" on Whatsapp</p>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16">
         <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"></path>
     </svg>
 </button>
@@ -100,15 +100,15 @@ function Home() {
                 <div className="foot-logo">
                     <img src={logo} alt="Logo" className="logo" />
                 </div>
-                <div class="grid-container">
-                    <a href="#link1" class="grid-item">HOME</a>
-                    <a href="#link2" class="grid-item"> BLOG</a>
-                    <a href="#link3" class="grid-item">FAQ</a>
-                    <a href="#link4" class="grid-item">ABOUT</a>
-                    <a href="#link5" class="grid-item">SERVICES</a>
-                    <a href="#link6" class="grid-item">CONTACT</a>
+                <div className="grid-container">
+                    <a href="#link1" className="grid-item">HOME</a>
+                    <a href="#link2" className="grid-item"> BLOG</a>
+                    <a href="#link3" className="grid-item">FAQ</a>
+                    <a href="#link4" className="grid-item">ABOUT</a>
+                    <a href="#link5" className="grid-item">SERVICES</a>
+                    <a href="#link6" className="grid-item">CONTACT</a>
                 </div>
-                <div class="foot">
+                <div className="foot">
                     <p >Company registered address :- H. No. 24 Rajpur Khurd, Chhattarpur, Delhi 110068</p>
                     <p>Branch office :- Lane No 1 - Dwarika Puram, Mothrowala Dehradun - 248001</p>
                     <p>info@c4capture.com | +91 9599499028 |+91 9599499047  </p>
